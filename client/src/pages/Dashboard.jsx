a4771a8fce63ae48e0ccf86eeb66af469058576d
Caleb Chiang <calebchiang1002@gmail.com>
2024-04-05 15:38:06 -0700

    Adjusted dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -9,7 +9,7 @@ function Dashboard() {
     const [transactions, setTransactions] = useState([]);
 
     return (
-        <div className="mx-auto w-full max-w-4xl py-8 px-4">
+        <div className="w-full max-w-4xl py-8 pl-2 pr-4 ml-[100px]">
             <Sidebar className="w-64 h-screen bg-gray-800 text-white fixed inset-y-0 left-0" />
             <div className="flex my-4 space-x-4">
                 <YearlyExpenses />
